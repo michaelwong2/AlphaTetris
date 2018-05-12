22b335626c039ee9e342cd759302b4131b67a55d
michaelwong2 <michaelwongait@gmail.com>
2018-05-12 16:38:23 -0400

    new helping functions for testing

--- api/move_test.py
+++ api/move_test.py
@@ -4,21 +4,31 @@ from api.bmat import Board_Matrix
 from api.block import Block
 from api.utils import encode_move, decode_move
 from tree_components import calculate_move
+from test_utils import format_bmat
 
 # test 1
-m1 = [[None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 1, 1, 1, 1], 
-	  [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 1, 1, 1, 1], 
-	  [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 4, 4], 
-	  [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 4, 4], 
-	  [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 2, 3], 
-	  [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 1, 3], 
-	  [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 5, 5, 5], 
-	  [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 1, 1, 1, 1, 1], 
-	  [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 1, 1, 1, 1, 1], 
-	  [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None]]
+m = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 5, 4, 4, 1, 0, 1, 0, 0, 0], 
+[0, 4, 0, 1, 4, 0, 1, 0, 0, 1], 
+[0, 3, 0, 1, 4, 1, 1, 0, 0, 1], 
+[0, 2, 0, 1, 3, 1, 3, 4, 4, 1], 
+[0, 1, 1, 1, 2, 1, 2, 4, 3, 1]]
 
-t1 = Board_Matrix()
-t1.set_matrix(m1)
+t1 = format_bmat(m)
 p1 = Block(1)
 p1.set_offset(0,16)
 
